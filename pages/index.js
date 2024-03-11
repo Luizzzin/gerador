4b9f15a98099f6dd210120745551b4026ebdfbbc
Luizzzin <luizhenribarbodias@gmail.com>
2024-03-11 16:59:16 -0300

    up
    up

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
+import Slider from "@react-native-community/slider";
 
 
 export function Home() {
@@ -8,19 +9,55 @@ export function Home() {
                 <Image source={require("../assets/logo.png")} style={ESTILO.logo} />
                 app na web
             </Text>
+            <View style={ESTILO.area}>
+                <Slider style={{ height: 50 }}
+                    minimumValue={6}
+                    maximumValue={20}
+                    minimumTrackTintColor="#ff0000"
+                    maximumTrackTintColor="#000"
+                    thumbTintColor="#392de9"
+                />
+            </View>
+            <TouchableOpacity style={ESTILO.button}>
+                <Text style={ESTILO.buttonText}>
+                    Gerar Senha
+                </Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
 const ESTILO = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: "#f3f3ff",
+        flex : 1,
+        backgroundColor: "#fff7",
         justifyContent: 'center',
         alignItems: 'center'
     },
     logo: {
-        marginBottom: 60
-    }
+        flex: 1,
+        marginBottom: 60,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    area: {
+        marginBottom: 14,
+        marginTop: 14,
+        width: "80%",
+        backgroundColor: "#FFF",
+        borderRadius: 8,
+        padding: 8
+    },
+    button: {
+        backgroundColor: "#392de9",
+        width: "70%",
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+    },
+    buttonText: {
+        color: "#FFF"
+    },
 
 })
--- routes.js
+++ routes.js
@@ -1,14 +1,41 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from './pages/index';
 import { PaginaSenhas } from './pages/paginaSenhas';
+import { Ionicons } from '@expo/vector-icons/';
 
 const Tab = createBottomTabNavigator();
 
 export function Routes() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="home" component={Home} />
-            <Tab.Screen name="paginaSenhas" component={PaginaSenhas} />
+            <Tab.Screen
+                name="home"
+                component={Home}
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return (<Ionicons size={35} color={"#392de9"} name="home" />)
+                        }
+                        return (<Ionicons size={25} color={"#392de9"} name="home-outline" />)
+                    }
+                }}
+            />
+            <Tab.Screen
+                name="paginaSenhas"
+                component={PaginaSenhas}
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return (<Ionicons size={35} color={"#392de9"} name="lock-closed" />)
+                        }
+                        return (<Ionicons size={25} color={"#392de9"} name="lock-closed-outline" />)
+                    }
+                }}
+            />
         </Tab.Navigator>
     )
 }
