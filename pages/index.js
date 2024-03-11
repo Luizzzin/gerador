7274ff7cabe54d5b8d5d4e9a5b052c7c7067e856
Luizzzin <luizhenribarbodias@gmail.com>
2024-03-11 10:34:00 -0300

    alguns passos
    steps

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet, Image } from "react-native";
+
+
+export function Home() {
+  return (
+    <View style={ESTILO.container}>
+      <Text>
+      <Image source={require("./assets/logo.png")} style={ESTILO.logo}/>
+        app na web
+      </Text>
+    </View>
+  )
+}
+
+const ESTILO = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f3f3ff",
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logo: {
+    marginBottom: 60
+  }
+
+})
